2b61f726c1a1633c6b63bc31e94371c94b44ee99
abdulhamid-mazroua <abdulhamidmazroua@gmail.com>
2026-07-04 19:47:38 +0400

    feat: add custom prompt provider for enhanced command line interface

--- src/main/java/org/hameed/hameedmoneycli/HameedMoneyCliApplication.java
+++ src/main/java/org/hameed/hameedmoneycli/HameedMoneyCliApplication.java
@@ -1,8 +1,12 @@
 package org.hameed.hameedmoneycli;
 
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStyle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.shell.jline.PromptProvider;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -12,4 +16,8 @@ public class HameedMoneyCliApplication {
         SpringApplication.run(HameedMoneyCliApplication.class, args);
     }
 
+    @Bean
+    public PromptProvider promptProvider() {
+        return () -> new AttributedString("hmc:> ", AttributedStyle.DEFAULT.foreground(AttributedStyle.CYAN));
+    }
 }
